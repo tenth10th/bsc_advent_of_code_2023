a5998ab46e25ffb8890b172c935f6ff8140c5237
David Sturgis <david-sturgis@pluralsight.com>
2023-12-12 02:06:46 +0000

    WIP For BSC event on 12-11-2023

--- src/main.py
+++ src/main.py
@@ -1,6 +1,49 @@
-def main():
-    pass
+import string
+
+
+def main(lines):
+    numbers = list()
+    for line in lines:
+        numbers.append(parse_line(line))
+    return sum(numbers)
+
+def parse_line(line):
+    first_digit = ""
+    last_digit = ""
+
+    for character in line:
+        if character in string.digits:
+            first_digit = character
+            break
+
+    for character in reversed(line):
+        if character in string.digits:
+            last_digit = character
+            break
+
+    # print("First digit:", first_digit)
+    # print("Last digit:", last_digit)
+
+    string_number = first_digit + last_digit
+
+    # print("number:", repr(string_number))
+
+    actual_number = int(string_number)
+
+    return actual_number
+
+
+def load_lines_from_file():
+    with open("data/day_1_data.txt", "r") as f:
+        lines = f.readlines()
+    return lines
 
 
 if __name__ == "__main__":
-    main()
+    """
+    Ensure that the code only runs if call this file directly
+    e.g. `python src/main.py`
+    """
+    lines = load_lines_from_file()
+    total = main(lines)
+    print("total:", total)
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,5 +1,43 @@
-from src.main import main
+from src.main import main, parse_line, load_lines_from_file
+from pytest import mark
+
+TEST_DATA = """\
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet\
+"""
+
+TEST_LINES = TEST_DATA.split("\n")
+
+# 12, 38, 15, and 77. Adding these together produces 142.
+EXPECTED_VALUES = [12, 38, 15, 77]
+
+EXPECTED_TOTAL = 142
 
 
-def test_example():
-    main()
+def test_lines():
+    for line in TEST_LINES:
+        print(line)
+    assert len(TEST_LINES) == 4
+
+
+@mark.parametrize(
+    ["line", "expected_number"],
+    [
+        ("1abc2", 12),
+        ("1abc23", 13),
+    ],
+)
+def test_first_line(line, expected_number):
+    assert parse_line(line) == expected_number
+
+
+def test_main():
+    assert main(TEST_LINES) == EXPECTED_TOTAL
+
+
+@mark.expensive
+def test_main_full_data():
+    lines = load_lines_from_file()
+    assert main(lines) == 55_002
