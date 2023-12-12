69f4368c19945a99745bb41add0cb68660a5bbaa
David Sturgis <david-sturgis@pluralsight.com>
2023-12-12 02:44:19 +0000

    WIP on session 2023-12-11

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# This could contain code that affects imports...
+# But even empty, it helps Python (and other dev tools)
+# understand that this folder is a Python module, and
+# should allow imports!
+print("\n(Someone is importing stuff from main...)")
--- src/main.py
+++ src/main.py
@@ -1,13 +1,40 @@
 import string
+from typing import List, Iterable
+
+"""
+Some stuff we talked about:
+
+[] = square braces, or square brackets or just brackets
+() = Parenthesis or just parens
+{} = curly braces or just curlies
+
+x = foo(y)  # "assign x to the foo of y"
+
+Callable: Things that can be called, e.g. foo()
+    Functions, also Class Methods
+    x = Foo()
+    x.bar()
+
+{} = Dictonary literals {"foo": 1} or Set literals {1, 2}
+
+[] = Accessing by index [0] or key ["foo"]
+
+-> = "arrow operator", or sometimes "indirect"
+
+from collections import abc
+    Abstract Base Classes - Make it easy to create
+    your own List or Dictionary-like objects...
+"""
 
 
-def main(lines):
+def main(lines: Iterable[str]) -> int:
     numbers = list()
     for line in lines:
         numbers.append(parse_line(line))
     return sum(numbers)
 
-def parse_line(line):
+
+def parse_line(line: str) -> int:
     first_digit = ""
     last_digit = ""
 
@@ -33,7 +60,7 @@ def parse_line(line):
     return actual_number
 
 
-def load_lines_from_file():
+def load_lines_from_file() -> List[str]:
     with open("data/day_1_data.txt", "r") as f:
         lines = f.readlines()
     return lines
